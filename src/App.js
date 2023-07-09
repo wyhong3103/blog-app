4e7020bee471e47b038a13c2897e8c1aac43f940
wyhong3103 <wyhong3103@gmail.com>
2023-07-09 20:49:03 +0800

    Add chakra provider

--- src/App.js
+++ src/App.js
@@ -4,32 +4,35 @@ import { Blog } from "./page/Blog";
 import { Create } from "./page/Create";
 import { Login } from "./page/Login";
 import { Register } from "./page/Register";
+import { ChakraProvider } from '@chakra-ui/react'
 
 export const App = () => {
     return(
-        <BrowserRouter>
-            <Routes>
-                <Route 
-                    path='/'
-                    element={<Home/>}
-                />
-                <Route 
-                    path='/login'
-                    element={<Login/>}
-                />
-                <Route 
-                    path='/signup'
-                    element={<Register/>}
-                />
-                <Route 
-                    path='/blog/:id'
-                    element={<Blog/>}
-                />
-                <Route 
-                    path='/create'
-                    element={<Create/>}
-                />
-            </Routes>
-        </BrowserRouter>
+        <ChakraProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path='/'
+                        element={<Home/>}
+                    />
+                    <Route
+                        path='/login'
+                        element={<Login/>}
+                    />
+                    <Route
+                        path='/register'
+                        element={<Register/>}
+                    />
+                    <Route
+                        path='/blog/:id'
+                        element={<Blog/>}
+                    />
+                    <Route
+                        path='/create'
+                        element={<Create/>}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
     )
 };
